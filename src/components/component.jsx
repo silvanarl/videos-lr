3f08321040331a80c789591f7720c2c5ec971b0a
silvanarl <ramossilvana495@gmail.com>
2021-02-01 22:55:31 -0500

    destacado de videos primera vista mfirst

--- src/components/component.jsx
+++ src/components/component.jsx
@@ -4,19 +4,42 @@ import ReactPlayer from 'react-player';
 const Componente = () => {
     return (
         <>
-            <div className="container">
+        <section className="featured-video">
+            <div className="featured-video-main">
                 <ReactPlayer
-                    url="https://www.youtube.com/watch?v=6NXnxTNIWkc"
+                    url="https://youtu.be/yKf9aUIxdb4"
                     controls
-                >
-                    
-                </ReactPlayer>
-                
+                    width="100%"
+                    heigth="250px"
+                >    
+                </ReactPlayer> 
             </div>
+            <div className="featured-video-info">
+                <span>Tecnología</span>
+                <h1>Los Sudokus resurgen: los momentos '¡eureka!' de vídeos que los resuelven son la nueva sensación de YouTube</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium error voluptate, suscipit voluptas sequi totam beatae similique delectus commodi, reprehenderit necessitatibus molestiae nesciunt? Aperiam earum vitae asperiores possimus ad dolores.</p>
+            </div>
+            <div>
+                Fecha |
+                <div>
+                    Compartir:
+                </div>
+            </div>
+        </section>
             <style jsx="true">
                 {`
-                .container {
-                    background-color: salmon;
+                .featured-video {
+                    max-width: 1000px;
+                    margin:auto;
+                    background-color: #000;
+                    display: flex;
+                    color:#fff;
+                    padding: 1.5rem;
+                    justify-content: center;
+                    flex-direction: column;
+                }
+                .featured-video-main{
+                    width: 100%
                 }
                 `}
             </style>
