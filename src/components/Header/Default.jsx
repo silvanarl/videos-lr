e633bb5287217596839315459420691e71012c49
Mauro Rivera <mauro.rivera@glr.pe>
2021-02-03 15:26:29 -0500

    Added header

---
+++ src/components/Header/Default.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import ReactPlayer from "react-player";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const currentDay = new Date().getDay();
+    let currentMonthFinal;
+    const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    if (!isNaN(currentMonth) && currentMonth >= 1 && currentMonth <= 12) {
+        currentMonthFinal = months[currentMonth];
+    }
+
+    return (
+        <>
+            <header>
+                <ul className="header container">
+                    <li className="header-item">
+                        <Link to="/">Noticias</Link>
+                    </li>
+                    <li className="header-item">
+                        <Link to="/home">Política</Link>
+                    </li>
+                    <li className="header-item">
+                        <Link>Sociedad</Link>
+                    </li>
+                    <li className="header-item">
+                        <Link>Mundo</Link>
+                    </li>
+                    <li className="header-item">
+                        <Link>Turismo</Link>
+                    </li>
+                    <li className="header-item">
+                        <Link>Recetas</Link>
+                    </li>
+                    <li className="header-item">
+                        <Link>Videojuego</Link>
+                    </li>
+                    <li className="header-item">
+                        <Link>Tecnología</Link>
+                    </li>
+                    <li className="header-item">
+                        <Link>Espectáculos</Link>
+                    </li>
+                    <li className="header-item">
+                        <Link>Cine y Series</Link>
+                    </li>
+                </ul>
+            </header>
+            <style jsx="true">{`
+                .header {
+                    margin: 0 auto;
+                    padding: 0;
+                    justify-content: center;
+                    list-style: none;
+                    position: relative;
+                    overflow-x: auto;
+                    text-align: center;
+                    white-space: nowrap;
+                }
+                header {
+                    position: relative;
+                }
+                .header::-webkit-scrollbar {
+                    width: 4px; /* width of the entire scrollbar */
+                }
+
+                .header::-webkit-scrollbar-track {
+                    background: #252525; /* color of the tracking area */
+                }
+
+                .header::-webkit-scrollbar-thumb {
+                    background-color: #333; /* color of the scroll thumb */
+                }
+                header:before {
+                    width: 100vw;
+                    position: absolute;
+                    top: 0;
+                    left: 50%;
+                    margin-left: -50vw;
+                    background: #313131;
+                    height: 100%;
+                    content: "";
+                    z-index: -1;
+                }
+                .header-item {
+                    display: inline-block;
+                    padding: 15px 13px;
+                }
+                .header-item a {
+                    font-size: 12px;
+                    color: #fff;
+                    text-decoration: none;
+                }
+            `}</style>
+        </>
+    );
+};
+export default Header; // importante <--
