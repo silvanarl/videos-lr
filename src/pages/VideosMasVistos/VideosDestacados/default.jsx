4ecd6b6c07306d82677a3e29cba9ea6ea0bc27cd
Elvis Saavedra <elvis.saavedra.german@gmail.com>
2021-02-03 16:01:51 -0500

    se agrego estilos a los videos

--- src/pages/VideosMasVistos/VideosDestacados/default.jsx
+++ src/pages/VideosMasVistos/VideosDestacados/default.jsx
@@ -1,19 +1,76 @@
 import React from 'react';
+import { add } from 'ramda';
 import { Link } from 'react-router-dom';
 import ReactPlayer from 'react-player';
+
+let currentDay = new Date();
 
 const VideosDestacados = ({ notice }) => {
   return (
     <div key={notice.id} className="content-notice">
-      <ReactPlayer url={notice.video} controls={true} playing={false} />
+      <ReactPlayer className="content-notice__videos" url={notice.video} controls={true} playing={false} />
       <Link to={'/' + notice.id}>
-        <p className="title">{notice.title}</p>
+        <h3 className="title">{notice.title}</h3>
       </Link>
+      <div className="content-notice__div">
+        <h4>Tendencias</h4>
+        <div>
+          {currentDay.getDate() + '/' + add(parseInt(currentDay.getMonth()), 1) + '/' + currentDay.getFullYear()}
+        </div>
+      </div>
       <style jsx="true">
         {`
-          .content-notice div {
+          .content-notice {
+            max-width: 100%;
+          }
+          .content-notice__videos {
+            margin: 0 auto;
             width: 80vw !important;
             height: 160px !important;
+          }
+          .content-notice a {
+            text-decoration: none;
+            color: black;
+          }
+          .content-notice a h3 {
+            width: 80vw;
+            margin: 15px auto 0;
+            padding-bottom: 12px;
+            border-bottom: 1px solid #d2d2d2;
+            font-family: Roboto;
+            font-size: 16px;
+            line-height: 18px;
+            font-weight: 700;
+          }
+          .content-notice__div {
+            display: flex;
+            flex direction: row;
+            justify-content: space-between;
+            // align-items: flex-start;
+          }
+          .content-notice__div h4 {
+            align-self: flex-start;
+            margin: 12px 0 auto 0;
+            font-family: Roboto;
+            font-size: 14px;
+            line-height: 16px;
+            font-weight: 700;
+            color: #f82d2e;
+          }
+          .content-notice__div div  {
+            margin: 12px 0 auto 0;
+            font-family: Roboto;
+            font-size: 14px;
+            line-height: 16px;
+            font-weight: 700;
+          }
+          @media only screen and (min-width: 800px) {
+           .content-notice__videos {
+            width: 40vw !important;
+            height: 190px !important;
+          }
+           .content-notice a p {
+            width: 40vw;   
           }
         `}
       </style>
--- src/pages/VideosMasVistos/default.jsx
+++ src/pages/VideosMasVistos/default.jsx
@@ -4,17 +4,24 @@ import VideosDestacados from './VideosDestacados/default';
 const VideosMasVistos = ({ info }) => {
   return (
     <div className="content-page-notice">
+      <h2>Te Recomendamos</h2>
       {info.map((notice, index) => {
         if (index < 3) return <VideosDestacados key={index} notice={notice} />;
       })}
       <style jsx="true">
         {`
           .content-page-notice {
+            max-width: 100%;
             display: grid;
             grid-template-columns: 1fr;
-            grid-gap: 15px;
-            width: 80vw;
-            margin: 10px auto;
+            grid-gap: 30px;
+            align-items: center;
+            justify-items: center;
+            // width: 80vw;
+            // margin: 10px auto;
+          }
+          @media only screen and (min-width: 800px) {
+            grid-template-columns: repeat(2, 1fr);
           }
         `}
       </style>
